8708a8cf4786b8db997cf03e2b27363d1bf1b88f
Reema shaik <162030772+Reema143786@users.noreply.github.com>
2025-01-16 13:05:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -22,11 +22,87 @@ const quotes=[
 "It always seems impossible until it’s done.",
 "Do what you can, with what you have, where you are.",
 ];
-function generatequote(){
-const text=document.querySelector('p');
-const index=Math.floor(Math.random()*quotes.length);
-text.textContent=quotes[index];
 
-};
-setInterval(generatequote,2000);
+// It changes randomly
 
+
+// function generatequote(){
+// const text=document.querySelector('p');
+// const index=Math.floor(Math.random()*quotes.length);
+// text.textContent=quotes[index];
+
+// };
+// setInterval(generatequote,2000);
+
+
+
+const color1 = [
+    "#FF0000",
+    "#008000",
+    "#0000FF",
+    "#FFFF00",
+    "#00FFFF",
+    "#FF00FF",
+    "#000000",
+    "#808080",
+    "#C0C0C0",
+    "#800000",
+    "#808000",
+    "#800080",
+    "#008080",
+    "#000080",
+    "#00FF00",
+    "#FFC0CB",
+    "#A52A2A",
+    "#FFA500",
+    "#FFD700"
+  ];
+  
+
+
+function bodybackcolor(){
+const colors=document.querySelector('body');
+const index1=Math.floor(Math.random()*color1.length);
+colors.style.backgroundColor=color1[index1];
+}
+setInterval(bodybackcolor,2000);
+
+
+
+const button=document.querySelector('button');
+// click,dblclick ,mousemove ,mouseover
+
+
+button.addEventListener('click',(event)=>{
+    const text=document.querySelector('p');
+    const index=Math.floor(Math.random()*quotes.length);
+    text.textContent=quotes[index];
+    console.log(event.target) //button is target //here it takes which tag is target
+    console.log(event.type) // dblclick,click,mouseover etc...
+    console.log(event.clientX) // [x axis ]at which point we click [exact position it shows]
+    console.log(event.clientY); // clickable position of Y 
+})
+
+// key board events [keydown,keyup]
+
+// addEventListener('keyup' ,()=>{
+//     const text=document.querySelector('p');
+//    const index=Math.floor(Math.random()*quotes.length);
+//     text.textContent=quotes[index];
+
+// })
+
+
+// From the above code if i pressing any key that quote [or] text is changing  But I want an o/p like this: If I am pressing the suotable key then only the text[or] qoute has to change
+
+
+
+// addEventListener('keydown',(event)=>{
+//     // console.log(event);  // event is a object whick keep many keyboard,mouse etc.. related events [when we trigger[or] press an event also it shows who like whick key does trigger]
+//     if(event.key==="Enter"){
+//         const text=document.querySelector('p');
+//     const index=Math.floor(Math.random()*quotes.length);
+//     text.textContent=quotes[index];
+//     }
+// console.log(event.target) // which tag is target here body is target because here we dont take  button.addEventListener('keydown',(event)=>{ if we take this it return target as button
+// })
